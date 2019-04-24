f3801bb76c3a658c0acda5ed3e2a35917b7367a6
deltagare <deltagare@LEXJONKAF32>
2019-04-24 11:44:27 +0200

    Add project files.

---
+++ WebAppMVCRecap/DbInitializer.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using WebAppMVCRecap.Models;
+
+namespace WebAppMVCRecap
+{
+    internal class DbInitializer//++
+    {
+        internal static void Initialize(AppDbContext context, Microsoft.AspNetCore.Identity.RoleManager<Microsoft.AspNetCore.Identity.IdentityRole> roleManager, Microsoft.AspNetCore.Identity.UserManager<Microsoft.AspNetCore.Identity.IdentityUser> userManager)
+        {
+            if (!roleManager.RoleExistsAsync("Admin").Result)
+            {
+                IdentityRole role = new IdentityRole("Admin");
+
+                roleManager.CreateAsync(role).Wait();
+            }
+
+            if (!roleManager.RoleExistsAsync("NormalUser").Result)
+            {
+                IdentityRole role = new IdentityRole("NormalUser");
+
+                roleManager.CreateAsync(role).Wait();
+            }
+
+            //---------------------------------------------------------------------------------
+
+            if (userManager.FindByNameAsync("Guru").Result == null)
+            {
+                IdentityUser user = new IdentityUser();
+                user.Email = "guru@admin.se";
+                user.UserName = "Guru";
+
+                var result = userManager.CreateAsync(user,"Password!123").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user,"Admin").Wait();
+                }
+            }
+
+            if (userManager.FindByNameAsync("Alle").Result == null)
+            {
+                IdentityUser user = new IdentityUser();
+                user.Email = "alle@admin.se";
+                user.UserName = "Alle";
+
+                var result = userManager.CreateAsync(user, "Password!123").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "NormalUser").Wait();
+                }
+            }
+
+            //---------------------------------------------------------------------------------
+
+            //don't forget
+            //context.SaveChanges(); 
+        }
+    }
+}
---
+++ WebAppMVCRecap/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WebAppMVCRecap.Models;
+
+namespace WebAppMVCRecap
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    RoleManager<IdentityRole> roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();//++
+                    UserManager<IdentityUser> userManager = services.GetRequiredService<UserManager<IdentityUser>>();//++
+
+                    var context = services.GetRequiredService<AppDbContext>();
+
+                    DbInitializer.Initialize(context, roleManager, userManager);//++
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
